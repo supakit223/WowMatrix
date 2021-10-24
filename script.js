952f548d934365e4e990f595948e3c79de950c06
supakit223 <supakit223@gmail.com>
2021-10-24 13:29:35 +0700

    ok

---
+++ script.js
@@ -0,0 +1,33 @@
+const amount = 1000
+let array = []
+for (let i = 1; i <= amount; i++) {
+    for (let j = 1; j <= amount; j++) {
+        for (let k = 1; k <= amount; k++) {
+            array[0] = i
+            array[1] = j
+            array[2] = k            
+            if (i*i == j*j + k*k && euclid_gcd_many(array) == 1) {
+                console.log(`${i} , ${j} , ${k}`)
+            }
+        }
+    }
+}
+// function หา ห.ร.ม.
+function euclid_gcd_two(number1,number2) {
+    let ses = number1%number2
+    if (ses > 0) {
+        return euclid_gcd_two(number2,ses)
+    }else{
+        return number2        
+    }
+}
+function euclid_gcd_many(array) { 
+    let i = 2        
+    let length = array.length  
+    let gcd = euclid_gcd_two(array[i-2],array[i-1])          
+    while (i < length) {        
+        gcd = euclid_gcd_two(gcd,array[i])
+        i++            
+    }
+    return gcd     
+}
