8fdc2ed62d7d2f5afc53619ad7dbd8f772bf9edd
supakit223 <supakit223@gmail.com>
2022-03-31 05:05:29 +0700

    first commit

--- script.js
+++ script.js
@@ -1,38 +1,27 @@
-function showPythagoras() { 
+function showWow() { 
     const amount = document.getElementById("amount").value    
-    // document.getElementById("show").innerHTML = amount  
-    let array = []
-    let show = ""
-    for (let i = 1; i <= amount; i++) {
-        for (let j = 1; j <= amount; j++) {
-            for (let k = 1; k <= amount; k++) {
-                array[0] = i
-                array[1] = j
-                array[2] = k           
-                if (k*k == i*i + j*j && i <= j && euclid_gcd_many(array) == 1) {
-                    show = show + `${i} , ${j} , ${k} <br>`                    
+    let show= ""    
+    let count = 0
+    for (let a = 1; a <= amount; a++) {
+        for (let b = 1; b <= amount; b++) {
+            for (let c = 1; c <= amount; c++) {      
+                for (let d = 1; d <= amount; d++) {     
+                    for (let e = 1; e <= amount; e++) {     
+                        for (let f = 1; f <= amount; f++) {  
+                            for (let g = 1; g <= amount; g++) {   
+                                for (let h = 1; h <= amount; h++) {    
+                                    if (a*e+b*g == 10*a+e && a*f+b*h == 10*b+f && c*e+d*g == 10*c+g && c*f+d*h == 10*d+h ) {
+                                        show = show + `${a} , ${b} <br> ${c} , ${d} <br> ${e} , ${f} <br> ${g} , ${h} <br><br>`                    
+                                        count++
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
     }
-    document.getElementById("show").innerHTML = show
-}
-// function หา ห.ร.ม.
-function euclid_gcd_two(number1,number2) {
-    let ses = number1%number2
-    if (ses > 0) {
-        return euclid_gcd_two(number2,ses)
-    }else{
-        return number2        
-    }
-}
-function euclid_gcd_many(array) { 
-    let i = 2        
-    let length = array.length  
-    let gcd = euclid_gcd_two(array[i-2],array[i-1])          
-    while (i < length) {        
-        gcd = euclid_gcd_two(gcd,array[i])
-        i++            
-    }
-    return gcd
+    document.getElementById("show1").innerHTML = count
+    document.getElementById("show2").innerHTML = show
 }
